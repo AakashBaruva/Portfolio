320d7468590b2622407bdbe10a5bb1b498b26365
AakashBaruva <aakashbaruva@gmail.com>
2024-02-02 00:43:44 -0500

    Revert "final 1"
    This reverts commit 14a456a0afbb19af2787cb5ebd6ebb48fa552b21.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,3 @@
-const { resolve } = require('path')
-
 export default {
     root: 'src/',
     publicDir: '../static/',
@@ -11,14 +9,6 @@ export default {
     },
     build:
     {
-        rollupOptions: {
-            input: {
-              main: resolve(__dirname, 'index.html'),
-              bigger: resolve(__dirname, 'bigger/bigger.html'),
-              big: resolve(__dirname, 'big/big.html'),
-              small: resolve(__dirname, 'small/bigger.html'),
-              smaller: resolve(__dirname, 'smaller/bigger.html'),
-            }},
         outDir: '../dist', 
         emptyOutDir: true, 
         sourcemap: true 
